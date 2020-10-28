769f2c0532668ea104d5fdd712bdb942df53d0e9
André Rodrigues <andre88.rg@gmail.com>
2020-10-28 20:47:12 -0300

    OrderItem, many-to-many association with extra attributes PART-1

--- src/main/java/com/example/demo/services/OrderService.java
+++ src/main/java/com/example/demo/services/OrderService.java
@@ -1,7 +1,6 @@
 package com.example.demo.services;
 
 import com.example.demo.entities.Order;
-import com.example.demo.entities.User;
 import com.example.demo.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
