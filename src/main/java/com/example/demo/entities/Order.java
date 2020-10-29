86692d280bdb254406433b366acd5149df2db86c
André Rodrigues <andre88.rg@gmail.com>
2020-10-28 22:03:23 -0300

    Subtotal & Total methods

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -85,6 +85,14 @@ public class Order {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+       Double sum = 0.0;
+       for (OrderItem x : items){
+           sum += x.getSubTotal();
+       }
+       return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/com/example/demo/entities/Payment.java
+++ src/main/java/com/example/demo/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.example.demo.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Payment implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
